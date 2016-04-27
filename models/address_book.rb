05cd766089e6f206f984d386bc6d9fc86e25fdb1
Leah <leahstuckey@me.com>
2016-04-27 09:53:13 -0500

    models/address_book.rb

--- models/address_book.rb
+++ models/address_book.rb
@@ -20,7 +20,7 @@ def add_entry(name, phone_number, email)
 
     entries.insert(index,Entry.new(name, phone_number, email))
   end
-
+  
   def import_from_csv(file_name)
     csv_text = File.read(file_name)
     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
@@ -28,5 +28,26 @@ def add_entry(name, phone_number, email)
       row_hash = row.to_hash
       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
+  end
+
+  def binary_search(name)
+
+    lower = 0
+    upper = entries.length - 1
+
+    while lower <= upper
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+
+    return nil
   end
 end
