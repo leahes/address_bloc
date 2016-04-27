0978367901284052173112a03a040ecd228e9c42
Leah <leahstuckey@me.com>
2016-04-27 10:37:01 -0500

    models/address_book.rb

--- models/address_book.rb
+++ models/address_book.rb
@@ -20,7 +20,7 @@ def add_entry(name, phone_number, email)
 
     entries.insert(index,Entry.new(name, phone_number, email))
   end
-  
+
   def import_from_csv(file_name)
     csv_text = File.read(file_name)
     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
@@ -28,6 +28,16 @@ def add_entry(name, phone_number, email)
       row_hash = row.to_hash
       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
+  end
+
+  def iterative_search(name)
+    @entries.each do |entry|
+      if entry.name == name
+        return entry
+      end
+    end
+    
+    return nil
   end
 
   def binary_search(name)
